b232cfb85b8995e63a0e25c3b3b504ea4a32616d
Beni Valotker <BeniV@traffilog.com>
2020-02-20 09:27:27 +0200

    version 1.0

---
+++ lambda_function.py
@@ -0,0 +1,20 @@
+#/path/path/
+#title           :Title 
+#description     :Description.
+#update date     :01/01/2020 12:10
+#version         :1.0
+#changes         :new version changes description.
+#python_version  :3.6  
+#==============================================================================
+
+import os
+import sys
+import json
+import config
+
+def lambda_handler(event, context):
+    try:
+        pass
+    except Exception as ex:
+        pass    
+    
